287b810b71ce1fcbae76a0e7375050f4b7ba46ad
Henrik Angelskar <henrik.angelskar@soprasteria.com>
2024-12-04 11:49:21 +0100

    latest txt

--- components/RSVP/RSVP.vue
+++ components/RSVP/RSVP.vue
@@ -2,9 +2,9 @@
   <div class="fancy-font pa-6 text-center">
     <div style="font-size: 1.05rem">
       <p class="mb-4">
-        Du är inbjuden till Sebastian och Amalies bröllop, 15-17 Augusti, RSVP
-        gärna så snart som möjligt. Mer info kommer snart här och på mail. Vi
-        ser fram imot att fira våran stoa dag med dig.
+        Du är inbjuden till Sebastian och Amalies bröllop, 15-17 Augusti, svara
+        gärna så snart som möjligt men senast 31 dec. Mer info kommer snart här
+        och på mail. Vi ser fram imot att fira våran stora dag med dig!
       </p>
 
       <p>Massor av kärlek,</p>
